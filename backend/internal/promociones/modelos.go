351af72247b15a4100041994f077af9784bc59af
matias-vilchesb <matias.75vilches@gmail.com>
2026-05-22 22:31:29 -0400

    importacion desde package inventario a package promociones

--- backend/internal/promociones/modelos.go
+++ backend/internal/promociones/modelos.go
@@ -1,5 +1,5 @@
 package promociones
-//import ("GPS_2026_1\backend\internal\inventario")
+import ("backend/internal/inventario")
 
 type Promocion struct {
 	ID 		  	string `gorm:"primaryKey;type:uuid;default;gen_random_uuid()" json:"id_promocion"`
@@ -11,5 +11,5 @@ type DetalleVenta struct{
 	PromocionID string `json:"id_promocion"`
 	Promocion 	Promocion`gorm:"foreignKey:PromocionID" json:"promocion,omitzero"`
 	EstaActivo 	bool `json:"esta_activa"`
-//	Producto 	inventario.Producto `gorm:"foreignKey:ProductoID" json:"producto,omitzero"`
+	Producto 	inventario.Producto `gorm:"foreignKey:ProductoID" json:"producto,omitzero"`
 }
