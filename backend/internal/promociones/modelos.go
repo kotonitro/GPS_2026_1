89bb97bfcaceb8cc96daef66aa0a8417c6531d0d
matias-vilchesb <matias.75vilches@gmail.com>
2026-05-27 16:45:21 -0400

    Funcionalidades CRUD para Promociones

--- backend/internal/promociones/modelos.go
+++ backend/internal/promociones/modelos.go
@@ -9,7 +9,7 @@ type Promocion struct {
 	Descuento 	float64 `json:"descuento"`
 }
 
-type DetalleVenta struct{
+type DetallePromocion struct{
 	PromocionID string `json:"id_promocion"`
 	Promocion 	Promocion`gorm:"foreignKey:PromocionID" json:"promocion,omitzero"`
 	EstaActivo 	bool `json:"esta_activa"`
