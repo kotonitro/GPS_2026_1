3be4562e80545eb0dd25aae8cad5a618ef3dc823
kotonitro <kotonitro554@gmail.com>
2026-06-02 23:02:18 -0400

    ADD: Validaciones de crear producto y actualizar producto

--- backend/internal/inventario/routes.go
+++ backend/internal/inventario/routes.go
@@ -4,10 +4,8 @@ import "github.com/gin-gonic/gin"
 
 // ConfigurarRutas registra todos los endpoints del módulo de inventario
 func ConfigurarRutas(api *gin.RouterGroup) {
-	// Creamos un subgrupo específico para inventario
 	grupo := api.Group("/inventario")
 	{
-		// Si la ruta base es /api, esto equivale a POST /api/inventario/
 		// Rutas para el CRUD de Productos
 		grupo.POST("/productos", CrearProducto)
 		grupo.GET("/productos", GetProductos)
