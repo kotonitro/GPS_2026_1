9355f5a9f1e056970fa11710bc1265804ba8c867
iTzFireFoxX <juanarevaloduran@gmail.com>
2026-06-14 13:01:11 -0400

    Package auth y config, funcion y ruta login

---
+++ backend/internal/auth/controller.go
@@ -0,0 +1,94 @@
+package auth
+
+import (
+	"backend/internal/empleados"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type AuthController struct {
+	db        *gorm.DB
+	jwtSecret string
+}
+
+func NewAuthController(db *gorm.DB, secret string) *AuthController {
+	return &AuthController{
+		db:        db,
+		jwtSecret: secret,
+	}
+}
+
+type LoginInput struct {
+	Usuario    string `json:"usuario" binding:"required"`
+	Contrasena string `json:"contrasena" binding:"required"`
+}
+
+type JWTClaims struct {
+	ID  string `json:"id_empleado"`
+	Rol string `json:"rol"`
+	jwt.RegisteredClaims
+}
+
+func (ctrl *AuthController) Login(c *gin.Context) {
+	var input LoginInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Debe enviar usuario y contraseña."})
+		return
+	}
+
+	var empleado empleados.Empleado
+
+	if err := ctrl.db.Where("usuario = ?", input.Usuario).First(&empleado).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Credenciales inválidas."})
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(empleado.Contrasena), []byte(input.Contrasena))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Credenciales inválidas."})
+		return
+	}
+
+	tiempoExpiracion := time.Now().Add(24 * time.Hour)
+	claims := JWTClaims{
+		ID:  empleado.ID,
+		Rol: empleado.Rol,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(tiempoExpiracion),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(ctrl.jwtSecret))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error interno al generar el token de autorización"})
+		return
+	}
+
+	c.SetCookie(
+		"auth",
+		tokenString,
+		int(24*time.Hour.Seconds()),
+		"/",
+		"localhost",
+		false,
+		true,
+	)
+
+	c.JSON(http.StatusOK, gin.H{
+		"mensaje": "Sesión iniciada exitosamente.",
+		"empleado": gin.H{
+			"id":      empleado.ID,
+			"usuario": empleado.Usuario,
+			"rol":     empleado.Rol,
+		},
+	})
+}
--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"backend/internal/auth"
 	"backend/internal/cajas"
 	"backend/internal/clientes"
+	"backend/internal/config"
 	"backend/internal/database"
 	"backend/internal/empleados"
 	"backend/internal/inventario"
@@ -14,17 +16,24 @@ import (
 
 func main() {
 
+	// variables de entorno
+	cfg := config.LoadConfig()
+
 	// base de datos
-	db := database.Init()
+	db := database.Connect(cfg)
 	database.Migrations(db)
 
 	// validaciones
 	validations.ValidationsConfig()
 
-	// rutas
+	// router
 	r := gin.Default()
+
+	// api
 	api := r.Group("/api")
 
+	// rutas
+	auth.RoutesConfig(api, db, cfg.JWTSecret)
 	cajas.RoutesConfig(api, db)
 	clientes.ConfigurarRutas(api)
 	empleados.RoutesConfig(api, db)
