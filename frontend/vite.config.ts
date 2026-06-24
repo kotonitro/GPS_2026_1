da19c511553607e1736aa3a5abc00ac1670df52c
iTzFireFoxX <juanarevaloduran@gmail.com>
2026-06-24 02:08:03 -0400

    pagina 404 simple

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,5 +18,6 @@ export default defineConfig({
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter()
 		})
-	]
+	],
+	server: { watch: { usePolling: true } }
 });
